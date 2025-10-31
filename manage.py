11f9d268ad21041f8163eaf73f64c1ea53bd41a4
ronewa.github.io <newanens@gmail.com>
2025-10-31 11:36:22 +0200

    Fix manage.py path

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+# Add saknew_backend to path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'saknew_backend'))
 
 def main():
     """Run administrative tasks."""
